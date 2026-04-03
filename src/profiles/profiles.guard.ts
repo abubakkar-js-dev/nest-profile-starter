05afc7a33e27d06eefe1d128bfc67293ef695962
abubakkar-dev <abubakkars182@gmail.com>
2026-04-03 19:21:31 +0600

    update: profile guard

--- src/profiles/profiles.guard.ts
+++ src/profiles/profiles.guard.ts
@@ -1,12 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class ProfilesGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> { 
-    const request = context.switchToHttp().getRequest();
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.headers['authorization'];
     if (authHeader && authHeader === 'Bearer mysecrettoken') {
       return true;
